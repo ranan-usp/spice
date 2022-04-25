1e077483c63654e731e141bfc9d1b3d12bdb7d78
ranan-usp <oe23ranan@ec.usp.ac.jp>
2022-04-25 17:06:44 +0900

    update

--- verilog-a/dff_convert.py
+++ verilog-a/dff_convert.py
@@ -26,26 +26,26 @@ with open(new_path,'w') as f:
             if 'DFLOP' in words:
                 name,d,dumm,clk,pre,clr,qb,q,dumm2 = words[:9]
                 name = 'XX' + name.split('A')[1]
-                new_words = [name,d,clk,q,qb,pre,clr,vdd,vss]
+                new_words = [name,d,clk,q,qb,pre,clr,vdd,vss,"VerilogA_DFlipFlop"]
                 words = [str(word) for word in new_words]
                 
 
             elif 'AND' in words:
                 name,dumm,A,dumm2,B,dumm3,QB,Q,dumm4 = words[:9]
                 name = 'XX' + name.split('A')[1]
-                new_words = [name,d,clk,q,qb,pre,clr,vdd,vss]
+                new_words = [name,d,clk,q,qb,pre,clr,vdd,vss,"VerilogA_AND"]
                 words = [str(word) for word in new_words]
 
             elif 'OR' in words:
                 name,A,B,C,dumm1,dumm2,QB,Q,dumm3 = words[:9]
                 name = 'XX' + name.split('A')[1]
-                new_words = [name,A,B,C,Q,QB]
+                new_words = [name,A,B,C,Q,QB,"VerilogA_OR"]
                 words = [str(word) for word in new_words]
 
             elif 'BUF' in words:
                 name,i,o = words[0],words[1],words[6]
                 name = 'XX' + name.split('A')[1]
-                new_words = [name,i,o]
+                new_words = [name,i,o,"VerilogA_inv"]
                 words = [str(word) for word in new_words]
 
         
